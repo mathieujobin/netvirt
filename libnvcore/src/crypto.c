dfed7969655a3afbea763f7a27165780e78b926a
Mathieu Jobin <mathieu@venuenext.com>
2017-06-06 13:37:40 -0400

    set this earlier

--- libnvcore/src/crypto.c
+++ libnvcore/src/crypto.c
@@ -214,9 +214,6 @@ int krypt_do_handshake(krypt_t *kconn, uint8_t *buf, size_t buf_data_size)
 	);
 
 	//SSL_peek(kconn->ssl, buf, 0);
-	// ADH ciphers are in security level 0
-	// https://www.openssl.org/docs/manmaster/man3/SSL_CTX_set_security_level.html
-	SSL_CTX_set_security_level(kconn->ctx, 0);
 	ret = SSL_do_handshake(kconn->ssl);
 	jlog(L_NOTICE, "(first handshake) SSL state: %s, return %d", SSL_state_string_long(kconn->ssl), ret);
 	jlog(L_NOTICE, "SSL_get_error(%d) => %d", ret, SSL_get_error(kconn->ssl, ret));
@@ -386,6 +383,9 @@ int krypt_secure_connection(krypt_t *kconn, uint8_t protocol, uint8_t conn_type,
 
 #if OPENSSL_VERSION_NUMBER >= 0x10100000L
 	SSL_CTX_set_dh_auto(kconn->ctx, 1);
+	// ADH ciphers are in security level 0
+	// https://www.openssl.org/docs/manmaster/man3/SSL_CTX_set_security_level.html
+	SSL_CTX_set_security_level(kconn->ctx, 0);
 #endif
 
 	SSL_set_bio(kconn->ssl, kconn->internal_bio, kconn->internal_bio);
